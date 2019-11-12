e82e17327bbebca1a2080b57dd7f57928d36175d
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:43:23 +0300

    Add files via upload

---
+++ 8/8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            string str = "Text";
+            foreach (char ch in str)
+            {
+                if (dict.ContainsKey(ch))
+                    dict[ch]++;
+                else
+                    dict.Add(ch, 1);
+            }
+            foreach (char key in dict.Keys)
+            {
+                t.Items.Add(key.ToString() + " - " + dict[key].ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
