151dfe22c5411bf5c6fca6e42aef5e9d29461c4f
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:42:03 +0300

    Add files via upload

---
+++ 6/6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Convert.ToString(Console.ReadLine()); 
+for (int i = 0; i < s.Length - 1; i+=3) 
+{ 
+
+Console.Write(s[i]+ ","); 
+
+} 
+Console.ReadKey(); 
+
+string c = Convert.ToString(Console.ReadLine()); 
+int x = s.Length; 
+if (s.Length > 5) 
+{ 
+Console.WriteLine(s[0] + "," + c[1] + "," + c[2] + "," + c[s.Length - 3] + "," + c[s.Length - 2] + "," + c[s.Length - 1]); 
+
+} 
+else 
+while(x>0){ 
+x -= 1; 
+Console.WriteLine(s[0]); 
+} 
+
+Console.ReadKey();
+
+        }
+    }
+}
