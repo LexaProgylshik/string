98f5a915c729bb911ebcb27e92654d9b3bd6653d
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:49:08 +0300

    Add files via upload

---
+++ 19/19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Login1, LOgin2, login3, loGIN4";
+            Console.WriteLine(str.ToLower());
+            string[] login = str.Split(',');
+                Console.WriteLine(login[0]);
+            Console.ReadKey();
+
+        }
+    }
+}
