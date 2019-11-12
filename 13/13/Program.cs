16449aa7c3904519196152da25e6f6b8a312feca
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:47:30 +0300

    Add files via upload

---
+++ 13/13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "asdakjdhqwopdqihasdfaskldjqwiodhaspodhwqiodhpasodjkxzcvjhasdhaskdh";
+            Console.WriteLine(s.All(c => c == 'a' || c =='b'));
+            Console.ReadKey();
+        }
+    }
+}
