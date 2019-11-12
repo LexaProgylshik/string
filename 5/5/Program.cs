073bae0b82a3ead0756b592e114f7ce9e865c296
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:41:31 +0300

    Add files via upload

---
+++ 5/5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "word of "; 
+                Console.WriteLine("s+", "+s+", "+s"); 
+                  Console.WriteLine("Количество символов: "+3*s.Length);
+                  Console.ReadKey();
+        }
+    }
+}
