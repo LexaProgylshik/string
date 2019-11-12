f09743035a3ee5c7be43ced448806ab2f4feb9ef
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:46:59 +0300

    Add files via upload

---
+++ 12/12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "a4n287b898";
+            char[] ch = s.ToCharArray();
+            var count = ch.Where((n) => n >= '0' && n <= '9').Count();
+            Console.WriteLine("Кол-во цифр в строке:" + count);
+            Console.ReadKey();
+        }
+    }
+}
