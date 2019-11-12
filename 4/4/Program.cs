8af213c0181c81dafd7dbb6f647ef651a1d5efe3
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:40:46 +0300

    Add files via upload

---
+++ 4/4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (s.Length > 5)
+                Console.WriteLine(s.Substring(0, 3) + s.Substring(s.Length - 3));
+            else
+                Console.WriteLine(new String(s[0], s.Length));
+            Console.ReadKey();
+        }
+    }
+}
