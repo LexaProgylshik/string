b88e8147676a78fb969eaedb280adf0ad953c668
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:48:01 +0300

    Add files via upload

---
+++ 15/15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Replace("xabc", "abc");
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
