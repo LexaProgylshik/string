74bedbfd270ee10bd3e70dfd06844e61ad2a80d8
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:46:24 +0300

    Add files via upload

---
+++ 11/ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "THE WITCHER 3";
+            Console.WriteLine(s.Substring(0,6));
+            
+            Console.ReadLine();
+
+        }
+    }
+}
