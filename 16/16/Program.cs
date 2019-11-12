b17fa365568d5762ac43f80e145c00151506d535
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:48:32 +0300

    Add files via upload

---
+++ 16/16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Сегодня мы с вами рассмотрели, как работать со строками в Си-шарп. Были описаны основные операторы и методы, которые используются для работы со строками";
+            Console.WriteLine(s.Substring(67,40));
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
