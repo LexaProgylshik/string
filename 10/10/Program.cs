3bc194146a2a4c4fca98fdfb981325cda694ea6d
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:45:43 +0300

    Add files via upload

---
+++ 10/10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string s = "abc, www";
+            Console.WriteLine(s.Replace("abc", "www"));
+            
+            Console.ReadLine();
+        }
+    }
+}
