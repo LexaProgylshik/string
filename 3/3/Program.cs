418558f5c7871ef26688b5ba727ddd51694baf84
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:40:01 +0300

    Add files via upload

---
+++ 3/3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = string.Empty;
+            int count = 0;
+            Console.WriteLine("Input string line");
+            inputLine = Console.ReadLine();
+            count = inputLine.Length;
+            Console.WriteLine("First:{0}, Last{1}", inputLine[0]. ToString(), inputLine[count - 1]);
+            Console.ReadKey();
+        }
+    }
+}
