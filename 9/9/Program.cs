2590e2fcb4403f87331529437fa4cc37d608abe0
LexaProgylshik <57666429+LexaProgylshik@users.noreply.github.com>
2019-11-12 14:45:07 +0300

    Add files via upload

---
+++ 9/9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Хочу покушать.Но денег нет";
+            Console.WriteLine(s.Substring(14));
+            Console.WriteLine(s.Substring(14));
+            Console.WriteLine(s.Substring(14));
+            Console.ReadKey();
+
+        }
+    }
+}
